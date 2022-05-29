5c6f3f1c7a2842ff1831cfd35b13ad8074129ea8
Furkan <88673219+TeharesFK@users.noreply.github.com>
2022-05-29 19:04:45 +0300

    Update database.php

--- feedback/config/database.php
+++ feedback/config/database.php
@@ -1,8 +1,8 @@
 <?php
-    define('DB_HOST','localhost');
-    define('DB_USER','Furkan');
-    define('DB_PASS','123456');
-    define('DB_NAME','php_dev');
+    define('DB_HOST','');
+    define('DB_USER','');
+    define('DB_PASS','');
+    define('DB_NAME','');
 
     $conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
